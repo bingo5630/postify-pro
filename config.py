4e362cdc994f205c1b50e7cb2cea13294eb84355
bingo5630 <binghh5@gmail.com>
2026-05-29 22:36:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,8 +34,6 @@ FORCE_PIC = os.environ.get("FORCE_PIC", "https://graph.org/file/d14344c65c52acc0
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
-PICS = (os.environ.get("PICS", "https://i.ibb.co/S43S67Pf/Picsart-26-05-29-05-43-05-096.png")).split() #Required
-
 PICS = (os.environ.get("PICS", "https://graph.org/file/8195f2ba9ec35e5673ae9-17a0119f790f64e3b3.jpg")).split() #Required
  
 #set your Custom Caption here, Keep None for Disable Custom Caption
