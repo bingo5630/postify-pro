5373aeb114e47d879848f5d2de27395acd773f7d
bingo5630 <binghh5@gmail.com>
2026-05-29 20:38:06 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -157,7 +157,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         user_link = f"https://t.me/{user.username}" if user.username else f"tg://openmessage?user_id={OWNER_ID}" 
         ownername = f"<a href={user_link}>{user.first_name}</a>" if user.first_name else f"<a href={user_link}>no name !</a>"
         await query.edit_message_media(
-            InputMediaPhoto("https://ibb.co/990hr6Mr",
+            InputMediaPhoto("https://graph.org/file/b8cfa92c88dc837eb0eb7-37dc1d2a8e992fb176.jpg",
                             ABOUT_TXT.format(
                                 botname = client.name,
                                 ownername = ownername, 
